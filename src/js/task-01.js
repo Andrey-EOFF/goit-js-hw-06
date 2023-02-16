cdae87bb14864e3293fa1037d1306aa059c35631
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-02-16 20:49:12 +0200

    task-2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -14,15 +14,3 @@ categoriesItems.forEach((item) => {
   );
 });
 
-
-// const categoriesList = document.querySelector("ul#categories");
-
-// const categoriesItems = categoriesList.querySelectorAll("li.item");
-// console.log(`Number of categories: ${categoriesItems.length}`);
-
-// for (const item of categoriesItems) {
-//   const categoriesName = item.querySelector("h2").textContent;
-//   const categoriesElements = item.querySelectorAll("li").length;
-//   console.log(`Category: ${categoriesName}`);
-//   console.log(`Elements: ${categoriesElements}`);
-// }
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const element = ingredients.map((ingredientName) => {
+  const ingredientsCreatItem = document.createElement("li");
+  ingredientsCreatItem.classList.add("item");
+  ingredientsCreatItem.textContent = ingredientName;
+
+  return ingredientsCreatItem;
+});
+
+ingredientsList.append(...element);
+
+console.log(element);
